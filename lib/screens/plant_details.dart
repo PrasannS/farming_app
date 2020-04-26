a4d7303c164cfd745a851830a03a3e1929ee66f3
lucaszcai <lucas.z.cai@gmail.com>
2020-04-26 10:10:21 -0500

    fixed vincent's doodoo

--- lib/screens/plant_details.dart
+++ lib/screens/plant_details.dart
@@ -1,3 +1,4 @@
+import 'package:farming_app/screens/post_page.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
@@ -12,177 +13,123 @@ class _PlantDetailsState extends State<PlantDetails> {
     return Scaffold(
       backgroundColor: Colors.grey.withOpacity(0.9),
       body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.white,
-            child: Column(
-            children: [
-              Stack(
+        padding: const EdgeInsets.all(8.0),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height
+          ),
+          child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                   child: Image.asset("assets/img/tomatoes.jpeg",
+                   fit: BoxFit.cover,),
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                        onTap: () => Navigator.pop(context),
+                        child: Padding(
+                            padding: EdgeInsets.all(2.0),
+                            child: const Icon(Icons.arrow_back))),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Card(
+                            elevation: 15,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Icon(Icons.not_interested, size: 30,
+                              color: Colors.red,),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+
+            Expanded(
+              child: Column(
                 children: [
                   Container(
-                     child: Image.asset("assets/img/tomatoes.jpeg",
-                     fit: BoxFit.cover,),
-                  ),
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Container(
-                      height: 100,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          // Where the linear gradient begins and ends
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          // Add one stop for each color. Stops should increase from 0 to 1
-                          colors: [
-                            // Colors are easy thanks to Flutter's Colors class.
-                            Colors.white.withOpacity(0.7),
-                            Colors.white.withOpacity(0.5),
-                            Colors.white.withOpacity(0.07),
-                            Colors.white.withOpacity(0.05),
-                            Colors.white.withOpacity(0.025),
-                          ],
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Container(),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: IconButton(icon: Icon(Icons.arrow_back,color: Colors.white,),
-                        onPressed: (){
-                          Navigator.pop(context);
-                        })
-                      ),
-                      Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: Card(
-                              elevation: 15,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20)
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Icon(Icons.not_interested, size: 30,
-                                color: Colors.red,),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                  Align(alignment: Alignment.bottomCenter,
-                  child: Container(
-                    height: 380,
-                    decoration: BoxDecoration(
-                      // Box decoration takes a gradient
-                      gradient: LinearGradient(
-                        // Where the linear gradient begins and ends
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        // Add one stop for each color. Stops should increase from 0 to 1
-                        colors: [
-                          Colors.white,
-                          Colors.white.withOpacity(0.09),
-                          Colors.white.withOpacity(0.01),
-                          Colors.white.withOpacity(0.00001),
-                        ],
-                      ),
-                      border: Border(
-                        bottom: BorderSide(width: 2)
-                      ),
-
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Container(),
-                    ),
-                  ),),
-                  Positioned(
-                    bottom: 0,
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      alignment: Alignment.bottomCenter,
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Text('The Good Stuff',
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.bottomCenter,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text('The Good Stuff',
                             style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 40
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40
                             ),),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Text('Cannabacaeae',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text('Cannabacaeae',
                             textAlign: TextAlign.start,
                             style: TextStyle(
-                              color: Colors.black.withOpacity(0.8), fontSize: 26, fontWeight: FontWeight.w100
+                                color: Colors.black.withOpacity(0.8), fontSize: 26, fontWeight: FontWeight.w100
                             ),),
-                          ),
-                          SizedBox(height: 15,)
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 15,)
+                      ],
                     ),
-                  )
-                ],
-              ),
-
-              Expanded(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                  ),
+                  Container(
+                    height: 1,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                        )
                     ),
-                    Container(
-                      height: 1,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                          )
-                      ),
-                      width: MediaQuery.of(context).size.width,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  SizedBox(
+                    height: 0,
+                  ),
+                  Container(
+                    child: CircularPercentIndicator(
+                      radius: 200,
+                      lineWidth: 20,
+                      animation: true,
+                      percent: 0.8,
+                      center: Icon(Icons.check_circle,
+                        color: Colors.green,
+                        size: 100,),
+                      circularStrokeCap: CircularStrokeCap.round,
+                      progressColor: Colors.green,
                     ),
-                    SizedBox(
-                      height: 0,
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Test'),
                     ),
-                    Container(
-                      child: CircularPercentIndicator(
-                        radius: 200,
-                        lineWidth: 20,
-                        animation: true,
-                        percent: 0.8,
-                        center: Icon(Icons.check_circle,
-                          color: Colors.green,
-                          size: 100,),
-                        circularStrokeCap: CircularStrokeCap.round,
-                        progressColor: Colors.green,
-                      ),
-                    ),
-                    Container(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text('Test'),
-                      ),
-                    ),
-                    SizedBox(width: 100,),
-                    Expanded(child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text('Yo yo yo what is up my dudes I am back here again with another fresh shipment of your favorite snack. Get your fill here!)',
-                      ),),
-                    ),
-                    Padding(
+                  ),
+                  SizedBox(width: 100,),
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text('Yo yo yo what is up my dudes I am back here again with another fresh shipment of your favorite snack. Get your fill here!)',
+                    ),),
+                  ),
+                  GestureDetector(
+                    onTap: () => print('go to post page'),
+                    child: Padding(
                       padding:
                       const EdgeInsets.all(9),
                       child: Material(
@@ -198,17 +145,18 @@ class _PlantDetailsState extends State<PlantDetails> {
                               style: TextStyle(
                                   color: Colors.black,
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 20.0),
+                                  fontSize: 20.0
+                              ),
                             ),
                           )),
                     ),
-                  ],
-                ),
-              )
-            ],
-            ),
+                  ),
+                ],
+              ),
+            )
+          ],
           ),
-          ),
+        ),
       ),
 
     );
